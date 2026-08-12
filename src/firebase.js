b9f5199e5dad90d4a09e03be244c06ee06f0f041
Jeisson-Velandia <60946059+Jeisson-Velandia@users.noreply.github.com>
2026-08-12 16:30:03 -0500

    Create firebase.js

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyB7sodBGjgxIEkeRKu-OzQeSvpMHh0Iu3k",
+  authDomain: "finanzas-b6264.firebaseapp.com",
+  projectId: "finanzas-b6264",
+  storageBucket: "finanzas-b6264.firebasestorage.app",
+  messagingSenderId: "201783265623",
+  appId: "1:201783265623:web:132ee7b504a23e9daed612",
+  measurementId: "G-WF96P73SEP"
+};
